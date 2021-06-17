af2d7a24ebb94ff4d152e4936872064aa9e13682
titov-andrei <titov_andrei@mail.ru>
2021-06-17 16:12:55 +0300

    Update db.sql

--- sql/db.sql
+++ sql/db.sql
@@ -9,22 +9,40 @@ CREATE TABLE IF NOT EXISTS `task_force`.`users` (
     `email` VARCHAR(45) NOT NULL,
     `password` VARCHAR(255) NOT NULL,
     `age` TINYINT NULL,
-    `city_id` INT NULL,
-    `role` TINYINT NULL DEFAULT 0,
-    `birthday_at` DATE NULL,
-    `phone` VARCHAR(11) NULL,
-    `skype` VARCHAR(255) NULL,
-    `telegram` VARCHAR(255) NULL,
-    `latitude` VARCHAR(255) NULL,
-    `longitude` VARCHAR(255) NULL,
     `last_activity_at` DATETIME NULL,
-    `biografy` TEXT NULL,
-    `avatar_id` INT NULL,
-    `is_public` INT NULL,
-    `created_at` DATETIME NOT NULL,
-    `updated_at` DATETIME NULL,
+    `created_at` TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    `updated_at` TIMESTAMP NULL,
     PRIMARY KEY (`id`),
     UNIQUE INDEX `uind_email_password` (`email` ASC, `password` ASC)
+);
+
+CREATE TABLE IF NOT EXISTS `task_force`.`communication` (
+    `phone` CHAR(11) NULL,
+    `skype` VARCHAR(255) NULL,
+    `telegram` VARCHAR(255) NULL,
+    PRIMARY KEY (`id`),
+);
+
+CREATE TABLE IF NOT EXISTS `task_force`.`location` (
+    `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    `city_id` INT NULL,
+    `latitude` VARCHAR(15) NULL,
+    `longitude` VARCHAR(15) NULL,
+    PRIMARY KEY (`id`),
+);
+
+CREATE TABLE IF NOT EXISTS `task_force`.`additional_information` (
+    `biografy` TEXT NULL,
+    `is_public`  TINYINT(1) NULL,
+    PRIMARY KEY (`id`),
+);
+
+CREATE TABLE IF NOT EXISTS `task_force`.`avatar` (
+    `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    `user_id` INT NOT NULL,
+    `file_id` INT NOT NULL,
+    PRIMARY KEY (`id`),
+    UNIQUE INDEX `uind_user_id_file_id` (`user_id` ASC, `file_id` ASC)
 );
 
 CREATE TABLE IF NOT EXISTS `task_force`.`user_categories` (
@@ -40,7 +58,7 @@ CREATE TABLE IF NOT EXISTS `task_force`.`user_photos` (
     `user_id` INT NOT NULL,
     `file_id` INT NOT NULL,
     PRIMARY KEY (`id`),
-    UNIQUE INDEX `uind_user_id_file_id` (`user_id` ASC, `file_id` ASC)
+    UNIQUE INDEX `uind_user_id_file_id` (`user_id` ASC, `avatar_id` ASC)
 );
 
 CREATE TABLE IF NOT EXISTS `task_force`.`tasks` (
@@ -54,50 +72,49 @@ CREATE TABLE IF NOT EXISTS `task_force`.`tasks` (
     `task_term_at` DATETIME NULL,
     `client_id` INT NOT NULL,
     `executor_id` INT NULL,
-    `status` INT NOT NULL,
-    `created_at` DATETIME NOT NULL,
-    `updated_at` DATETIME NULL,
+    `status` INT NOT NULL DEFAULT 1,
+    `created_at` TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    `updated_at` TIMESTAMP NULL,
     `city_id` INT NOT NULL,
-    PRIMARY KEY (`id`)
+    PRIMARY KEY (`id`),
+    UNIQUE INDEX `title_UNIQUE` (`title` ASC),
+    UNIQUE INDEX `category_id_UNIQUE` (`category_id` ASC),
+    UNIQUE INDEX `client_id_UNIQUE` (`client_id` ASC),
+    UNIQUE INDEX `status_UNIQUE` (`status` ASC)
 );
 
 CREATE TABLE IF NOT EXISTS `task_force`.`responses` (
     `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
-    `task_id` INT NULL,
+    `task_id` INT NOT NULL,
+    `user_id` INT NOT NULL,
     `price` INT UNSIGNED NULL,
     `comment` TEXT NULL,
-    `created_at` DATETIME NOT NULL,
-    `updated_at` DATETIME NULL,
+    `created_at` TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    `updated_at` TIMESTAMP NULL,
     PRIMARY KEY (`id`)
-);
-
-CREATE TABLE IF NOT EXISTS `task_force`.`task_file` (
-    `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
-    `task_id` INT NOT NULL,
-    `file_id` INT NOT NULL,
-    PRIMARY KEY (`id`),
-    UNIQUE INDEX `uind_task_id_file_id` (`task_id` ASC, `file_id` ASC)
 );
 
 CREATE TABLE IF NOT EXISTS `task_force`.`files` (
     `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    `task_id` INT NOT NULL,
     `title` VARCHAR(255) NOT NULL,
-    `type` VARCHAR(255) NOT NULL,
     `path` VARCHAR(255) NOT NULL,
     `user_id` INT NOT NULL,
-    `created_at` DATETIME NOT NULL,
-    `updated_at` DATETIME NULL,
+    `created_at` TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    `updated_at` TIMESTAMP NULL,
     PRIMARY KEY (`id`)
 );
 
 CREATE TABLE IF NOT EXISTS `task_force`.`messages` (
     `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
-    `chat_id` INT NOT NULL,
+    `task_id` INT NOT NULL,
     `author_id` INT NOT NULL,
     `comment` TEXT NOT NULL,
-    `created_at` DATETIME NOT NULL,
-    `updated_at` DATETIME NULL,
-    PRIMARY KEY (`id`)
+    `created_at` TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    `updated_at` TIMESTAMP NULL,
+    PRIMARY KEY (`id`),
+    UNIQUE INDEX `task_id_UNIQUE` (`task_id` ASC),
+    UNIQUE INDEX `author_id_UNIQUE` (`author_id` ASC)
 );
 
 CREATE TABLE IF NOT EXISTS `task_force`.`cities` (
@@ -107,16 +124,9 @@ CREATE TABLE IF NOT EXISTS `task_force`.`cities` (
     UNIQUE INDEX `city_UNIQUE` (`city` ASC)
 );
 
-CREATE TABLE IF NOT EXISTS `task_force`.`task_statuses` (
-    `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
-    `status_id` INT NULL,
-    PRIMARY KEY (`id`),
-    UNIQUE INDEX `status_UNIQUE` (`status_id` ASC)
-);
-
 CREATE TABLE IF NOT EXISTS `task_force`.`categories` (
     `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
-    `category` VARCHAR(45) NULL,
+    `category` VARCHAR(45) NOT NULL,
     PRIMARY KEY (`id`),
     UNIQUE INDEX `category_UNIQUE` (`category` ASC)
 );
@@ -126,38 +136,35 @@ CREATE TABLE IF NOT EXISTS `task_force`.`reviews` (
     `comment` TEXT NULL,
     `rating` TINYINT NOT NULL,
     `author_id` INT NOT NULL,
-    `created_at` DATETIME NOT NULL,
-    `updated_at` DATETIME NULL,
+    `created_at` TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    `updated_at` TIMESTAMP NULL,
     `user_id` INT NOT NULL,
-    PRIMARY KEY (`id`)
+    PRIMARY KEY (`id`),
+    UNIQUE INDEX `author_id_UNIQUE` (`author_id` ASC),
+    UNIQUE INDEX `user_id_UNIQUE` (`user_id` ASC)
 );
 
 CREATE TABLE IF NOT EXISTS `task_force`.`favorite_users` (
     `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
     `user_id` INT NOT NULL,
     `favoririte_user_id` INT NOT NULL,
-    `created_at` DATETIME NOT NULL,
-    `updated_at` DATETIME NULL,
+    `created_at` TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    `updated_at` TIMESTAMP NULL,
     PRIMARY KEY (`id`),
-    UNIQUE INDEX `uind_favoririte_user_id_user_id` (`favoririte_user_id` ASC, `user_id` ASC)
+    UNIQUE INDEX `uind_favoririte_user_id_user_id` (`favoririte_user_id` ASC, `user_id` ASC),
+    UNIQUE INDEX `user_id_UNIQUE` (`user_id` ASC),
+    UNIQUE INDEX `favoririte_user_id_UNIQUE` (`favoririte_user_id` ASC)
 );
 
 CREATE TABLE IF NOT EXISTS `task_force`.`user_settings` (
     `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
     `user_id` INT NOT NULL,
-    `new_response` TINYINT NULL DEFAULT 1,
-    `new_messages` TINYINT NULL DEFAULT 1,
-    `cancel_task` TINYINT NULL DEFAULT 1,
-    `task_start` TINYINT NULL DEFAULT 1,
-    `task completion` TINYINT NULL DEFAULT 1,
-    `created_at` DATETIME NOT NULL,
-    `updated_at` DATETIME NOT NULL,
+    `new_response` TINYINT(1) NOT NULL DEFAULT 1,
+    `new_messages` TINYINT(1) NOT NULL DEFAULT 1,
+    `cancel_task` TINYINT(1) NOT NULL DEFAULT 1,
+    `task_start` TINYINT(1) NOT NULL DEFAULT 1,
+    `task_completion` TINYINT(1) NOT NULL DEFAULT 1,
+    `created_at` TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    `updated_at` TIMESTAMP NULL,
     PRIMARY KEY (`id`)
-);
-
-CREATE TABLE IF NOT EXISTS `task_force`.`chats` (
-  `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
-  `recipient_id` INT NOT NULL,
-  `author_id` INT NOT NULL,
-  PRIMARY KEY (`id`)
 );
