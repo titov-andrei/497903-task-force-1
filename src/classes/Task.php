2b71a368e6ff731da5c882358f11f3a5a4240a41
titov-andrei <titov_andrei@mail.ru>
2021-06-23 19:09:40 +0300

    Update Task.php

--- src/classes/Task.php
+++ src/classes/Task.php
@@ -1,6 +1,11 @@
 <?php
 
-namespace TaskForce;
+namespace Task;
+
+use Task\Actions\ActionCancel;
+use Task\Actions\ActionRespond;
+use Task\Actions\ActionExecute;
+use Task\Actions\ActionRefuse;
 
 class Task
 {
@@ -11,28 +16,30 @@ class Task
     const STATUS_FAILED = 'failed';
 
     const ACTION_CANCEL = 'cancel';
-    const ACTION_REFUSE = 'refuse';
     const ACTION_RESPOND = 'respond';
+    const ACTION_REFUSE = 'refuse';
     const ACTION_EXECUTE = 'execute';
 
     private $status;
     private $executorId;
     private $customerId;
+    private $currentId;
 
-    public function __construct(int $executor, ?int $customer = null)
+    public function __construct(int $executor, ?int $customer = null, int $current, string $status)
     {
-        $this->status = self::STATUS_NEW;
+        $this->status = $status;
         $this->executorId = $executor;
         $this->customerId = $customer;
+        $this->currentId = $current;
     }
 
     public function getStatusMap()
     {
         return [
             self::STATUS_NEW => 'Новое',
-            self::STATUS_DONE => 'Выполнено',
             self::STATUS_CANCELED => 'Отменено',
             self::STATUS_IN_PROGRESS => 'В работе',
+            self::STATUS_DONE => 'Выполнено',
             self::STATUS_FAILED => 'Провалено',
         ];
     }
@@ -41,8 +48,8 @@ class Task
     {
         return [
             self::ACTION_CANCEL => 'Отменить',
-            self::ACTION_REFUSE => 'Отказаться',
             self::ACTION_RESPOND => 'Откликнуться',
+            self::ACTION_REFUSE => 'Отказаться',
             self::ACTION_EXECUTE => 'Выполнить',
         ];
     }
@@ -52,8 +59,8 @@ class Task
         $statusArray = [
             self::ACTION_CANCEL => self::STATUS_CANCELED,
             self::ACTION_RESPOND => self::STATUS_IN_PROGRESS,
+            self::ACTION_REFUSE => self::STATUS_FAILED,
             self::ACTION_EXECUTE => self::STATUS_DONE,
-            self::ACTION_REFUSE => self::STATUS_FAILED
         ];
 
         return $statusArray[$action] ?? null;
@@ -61,11 +68,37 @@ class Task
 
     public function getNextActionsByStatus($status)
     {
-        $actionsArray = [
-            self::STATUS_NEW => [self::ACTION_CANCEL, self::ACTION_RESPOND],
-            self::STATUS_IN_PROGRESS => [self::ACTION_EXECUTE, self::ACTION_REFUSE]
-        ];
+        switch ($status) {
+            case self::STATUS_NEW:
+                $actionCancel = new ActionCancel($this->executorId, $this->customerId, $this->currentId);
+                if ($actionCancel->getRights()) {
+                    return ($actionCancel);
+                } else {
+                    unset($actionCancel);
+                }
 
-        return $actionsArray[$status] ?? null;
+                $actionRespond = new ActionRespond($this->executorId, $this->customerId, $this->currentId);
+                if ($actionRespond->getRights()) {
+                    return ($actionRespond);
+                } else {
+                    unset($actionRespond);
+                }
+
+            case self::STATUS_IN_PROGRESS:
+                $actionRefuse = new ActionRefuse($this->executorId, $this->customerId, $this->currentId);
+                if ($actionRefuse->getRights()) {
+                    return ($actionRefuse);
+                } else {
+                    unset($actionRefuse);
+                }
+
+                $actionExecute = new ActionExecute($this->executorId, $this->customerId, $this->currentId);
+                if ($actionExecute->getRights()) {
+                    return ($actionExecute);
+                } else {
+                    unset($actionExecute);
+                }
+        }
+        return NULL;
     }
 }
