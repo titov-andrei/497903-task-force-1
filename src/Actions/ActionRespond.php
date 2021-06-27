0232e349ebe1213a64a69b40f5a4c9145508ec90
titov-andrei <titov_andrei@mail.ru>
2021-06-27 11:56:18 +0300

    Create ActionRespond.php

---
+++ src/Actions/ActionRespond.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace TaskForce\Actions;
+
+class ActionRespond extends Action
+{
+    public function getTitle(): string
+    {
+        return 'Откликнуться';
+    }
+
+    public function getName(): string
+    {
+        return 'respond';
+    }
+
+    public function getRights(int $executorId, int $customerId, int $currentId): bool
+    {
+        return true;
+    }
+}
