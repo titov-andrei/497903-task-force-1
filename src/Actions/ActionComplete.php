1d31887ffa16bdff325e48da5e7e207ad226d689
titov-andrei <titov_andrei@mail.ru>
2021-06-27 11:56:10 +0300

    Create ActionComplete.php

---
+++ src/Actions/ActionComplete.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace TaskForce\Actions;
+
+class ActionComplete extends Action
+{
+    public function getTitle(): string
+    {
+        return 'Выполнить';
+    }
+
+    public function getName(): string
+    {
+        return 'complete';
+    }
+
+    public function getRights(int $executorId, int $customerId, int $currentId): bool
+    {
+        return $this->currentId === $this->customerId;
+    }
+}
