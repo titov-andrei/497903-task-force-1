0ab58b2e77cc5f6f063a579a863a60a06de5d872
titov-andrei <titov_andrei@mail.ru>
2021-07-08 16:18:58 +0300

    Update CSVToSQLConverter.php

--- src/CSVToSQLConverter.php
+++ src/CSVToSQLConverter.php
@@ -6,59 +6,40 @@ use Exceptions\TaskForceException;
 
 class CSVToSQLConverter
 {
-    public $file;
-    public $directory;
-    public $CSVFileObject;
-
-    public function __construct(string $file, string $directory)
+    public function createSQLFromCSV($file, $directory): void
     {
-        $this->file = $file;
-        $this->directory = $directory;
-    }
-
-    public function createSQLFromCSV(): void
-    {
-        if (!file_exists($this->file)) {
+        if (!file_exists($file)) {
             throw new TaskForceException('Файл не существует');
         }
 
-        if (!file_exists($this->directory)) {
-            mkdir($this->directory);
+        if (!file_exists($directory)  && !mkdir($directory)) {
+            throw new TaskForceException('Не удалось создать дуректорию');
+            mkdir($directory);
         }
 
         try {
-            $this->CSVFileObject = new \SplFileObject($this->file, 'r');
+            $CSVFileObject = new \SplFileObject($file, 'r');
         } catch (TaskForceException $exception) {
             throw new TaskForceException('Не удалось открыть файл на чтение');
         }
-        
-        $this->CSVFileObject->setFlags(\SplFileObject::DROP_NEW_LINE | \SplFileObject::READ_AHEAD | \SplFileObject::SKIP_EMPTY | \SplFileObject::READ_CSV);
 
-        $newFile = basename($this->file, '.csv');
+        $CSVFileObject->setFlags(\SplFileObject::DROP_NEW_LINE | \SplFileObject::READ_AHEAD | \SplFileObject::SKIP_EMPTY | \SplFileObject::READ_CSV);
+
+        $headerData = implode(', ', $this->getHeaderData());
+
+        $values[] = sprintf("\t(%s)", implode(', ', array_map(function ($item) {
+            return "'{$item}'";
+        }, $CSVFileObject->fgetcsv(','))));
+
+        $newFile = basename($file, '.csv');
 
         try {
-            $SQLFileObject = new \SplFileObject("$this->directory/$newFile.sql", 'w');
+            $SQLFileObject = new \SplFileObject("$directory/$newFile.sql", 'w');
         } catch (TaskForceException $exception) {
             throw new TaskForceException('Не удалось создать или записать в файл');
         }
 
-        $headerData = implode(', ', $this->getHeaderData());
-
-        $SQLFileObject->fwrite("INSERT INTO $newFile ($headerData) VALUES");
-        $isFirstValues = true;
-
-        foreach ($this->getNextLine() as $line) {
-            $values = ",\n('" . implode("', '", $line) . "')";
-
-            if ($isFirstValues) {
-                $values = ltrim($values, ',');
-                $isFirstValues = false;
-            }
-
-            $SQLFileObject->fwrite($values);
-        }
-
-        $SQLFileObject->fwrite(';');
+        $SQLFileObject->fwrite("INSERT INTO $newFile ($headerData) VALUES $values;");
     }
 
     public function getHeaderData(): ?array
@@ -67,15 +48,5 @@ class CSVToSQLConverter
         $data = $this->CSVFileObject->fgetcsv();
 
         return $data;
-    }
-
-    private function getNextLine(): ?iterable {
-        $result = null;
-
-        while (!$this->CSVFileObject->eof()) {
-            yield $this->CSVFileObject->fgetcsv();
-        }
-
-        return $result;
     }
 }
